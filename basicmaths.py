a42e674c105ff963d5e84fd3c3198785ce7f4795
lakshita643 <lakshitadadhich482@gmail.com>
2026-02-01 13:55:54 +0530

    All +,-,*,/,%

--- basicmaths.py
+++ basicmaths.py
@@ -2,3 +2,11 @@ a=int(input("Enter value of a:"))
 b=int(input("Enter value of b:"))
 sum=a+b
 print(f"The sum of {a} and {b} is:{sum}")
+substract=a-b
+print(f"The substraction of {a} and {b} is:{substract}")
+multiply=a*b
+print(f"The multipliction of {a} and {b} is:{multiply}")
+devide=a/b
+print(f"The division of {a} and {b} is:{devide}")
+module=a%b
+print(f"The module of {a} and {b} is:{module}")
