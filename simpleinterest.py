b82b2dc511049adff4fcb109db479cfaabe85683
lakshita643 <lakshitadadhich482@gmail.com>
2026-02-03 10:18:07 +0530

    Add Simple Interest Program

---
+++ simpleinterest.py
@@ -0,0 +1,7 @@
+print("Simple Interest!")
+p=int(input("Enter principal amount :"))
+r=int(input("Enter rate of intrest :"))
+t=int(input("enter time in year:"))
+SI=(p*r*t)/100
+print(f"Simple Intrest is :{SI}")
+
