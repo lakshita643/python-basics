ff3afa349d0a84f797ce3d670c42c2d7c8b660a7
Lakshita Joshi <lakshitadadhich482@gmail.com>
2026-02-04 22:12:17 +0530

    Add list method program like- max(),min(),sum(),sort()

---
+++ sum2_list_method.py
@@ -0,0 +1,12 @@
+list1=[9,3,6,10,32,1,4,]
+print(list1)
+list1.sort()
+print("Sorting list in asending order is:",list1)
+list1.sort(reverse=True)
+print("Sorting list in desending order is:",list1)
+print("Maximum number of list is:",max(list1))
+print("Minimum number of ist is:",min(list1))
+print("Sum of the list is:",sum(list1))
+
+
+
