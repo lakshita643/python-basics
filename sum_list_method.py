21f156746ce89b42b036f97df1cf01abc15251b7
Lakshita Joshi <lakshitadadhich482@gmail.com>
2026-02-04 21:50:36 +0530

    Add program using sum list methods like append(),remove(),reverse().

---
+++ sum_list_method.py
@@ -0,0 +1,8 @@
+list1=["Smile","Hope","Dream","Peace"]
+print(list1)
+list1.append("Love")
+print("List1 add one more element is Love:\n",list1)
+list1.remove("Hope")
+print("List1 remove one element 'Hope':\n",list1)
+list1.reverse()
+print("List1 is reverse:\n",list1)
