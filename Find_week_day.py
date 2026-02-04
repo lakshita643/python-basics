6bacc22d7927188db85337576b53b96bf901a316
Lakshita Joshi <lakshitadadhich482@gmail.com>
2026-02-04 21:04:01 +0530

    Add program to find day of week using number(if-else).

---
+++ Find_week_day.py
@@ -0,0 +1,17 @@
+num=input("Enter week number(1-7):")
+if num=="1":
+    print("This day is Monaday.")
+elif num=="2":
+    print("This day is Tuesday.")
+elif num=="3":
+    print("This day is Wednesday.")
+elif num=="4":
+    print("This day is Thursday.")
+elif num=="5":
+    print("This day is Friday.")
+elif num=="6":
+    print("This day is Saturday.")
+elif num=="7":
+    print("This day is Sunday.")
+else:
+    print("This number is not valid!")
