e838505c105e3a6819a800f95394111bc58e4144
Lakshita Joshi <lakshitadadhich482@gmail.com>
2026-02-04 21:13:45 +0530

    Implement month day program using user input

---
+++ find_month.py
@@ -0,0 +1,27 @@
+num=input("Enter any Number to find the month(1-12):")
+if num=="1":
+    print("This month is January.")
+elif num=="2":
+    print("This month is February.")
+elif num=="3":
+    print("This month is March.")
+elif num=="4":
+    print("This month is April.")
+elif num=="5":
+    print("This month is May.")
+elif num=="6":
+    print("This month is June.")
+elif num=="7":
+    print("This month is July.")
+elif num=="8":
+    print("This month is August.")
+elif num=="9":
+    print("This month is September.")
+elif num=="10":
+    print("This month is Octomber.")
+elif num=="11":
+    print("This month is Novembor.")
+elif num=="12":
+    print("This month is December.")
+else:
+    print("This number is not valid!")
