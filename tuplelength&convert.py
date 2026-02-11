7426d2d5b43075b601a3d0e50b256120f69b3c19
Lakshita Joshi <lakshitadadhich482@gmail.com>
2026-02-11 22:20:49 +0530

    Add program to find the length of the tuple and convert tuple into list and also convert list into tuple.

---
+++ tuplelength&convert.py
@@ -0,0 +1,7 @@
+s=("Smile","Hope","Peace","Shine","Dream")
+print("Total number of elements in tuple is:",len(s))
+list1=list(s)
+print("Convert tuple into list:",list1)
+tuple2=tuple(list1)
+print("Convert list1 into tuple:",tuple2)
+
