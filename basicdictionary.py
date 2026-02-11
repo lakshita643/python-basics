ffdb01f8d688592f438d06e89cd64035dc7e898a
Lakshita Joshi <lakshitadadhich482@gmail.com>
2026-02-11 23:30:25 +0530

    Basic Dictionary Program.

---
+++ basicdictionary.py
@@ -0,0 +1,11 @@
+student={
+    "Name" :"Lakshita Joshi",
+    "Class" : "MCA 3rd Semester",
+    "Roll no.":16
+    }
+print(student)
+print(type(student))
+student["Name"]="Lovely"#Dictionary is Mutable. 
+print(student)
+student["Subject"]="Python"#Add new element in dictionary.
+print(student)
